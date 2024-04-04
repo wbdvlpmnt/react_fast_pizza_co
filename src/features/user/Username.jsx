30c3be7a0a675672fc3ad9cbbb7cdaae31fe4910
wbdvlpmnt <jai.gokhale5219@gmail.com>
2024-04-04 10:21:52 -0500

    using flexbox and css grid

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,3 +1,5 @@
 export default function Username() {
-  return <div className="hidden text-sm font-semibold">Jai</div>;
+  return (
+    <div className="hidden text-sm font-semibold md:block md:text-xl">Jai</div>
+  );
 }

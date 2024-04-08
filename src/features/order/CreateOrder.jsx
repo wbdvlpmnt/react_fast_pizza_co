c03454ffd0d74430fffce58eb67c43810f0c38bc
wbdvlpmnt <jai.gokhale5219@gmail.com>
2024-04-08 15:38:07 -0500

    styling the menu component

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -81,6 +81,7 @@ function CreateOrder() {
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <Button
+            type="primary"
             disabled={isSubmitting}
             className="font inline-block rounded-full bg-yellow-500 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
           >
